eb6b07fdf2a541ed94722a92494204d6f964768f
Tom Charnley <he4834@wayne.edu>
2023-01-27 01:37:41 -0500

    Addingauthor name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,5 +1,5 @@
-// Simple C++ program to display "Hello World" 
-// Header file for input output functions 
+// Simple C++ program to display "Hello World"  
+// Author: Tom Charnley <he4834@wayne.edu> 
 #include<iostream>  
   
 using namespace std; 
